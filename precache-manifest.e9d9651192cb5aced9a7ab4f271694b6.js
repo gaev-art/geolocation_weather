243e8270b7b9946675c877dd3e07e294fbdff674
gaev-art <forsakensr@gmail.com>
2020-10-01 13:38:52 +0300

    Updates

---
+++ precache-manifest.e9d9651192cb5aced9a7ab4f271694b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593817f203612113cc913fb782ce4a7b",
+    "url": "/geolocation_wether/index.html"
+  },
+  {
+    "revision": "0f22819b13b7cddb4881",
+    "url": "/geolocation_wether/static/css/main.c10f4c29.chunk.css"
+  },
+  {
+    "revision": "644a6bfa3d77aef16e59",
+    "url": "/geolocation_wether/static/js/2.171ecf6d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/geolocation_wether/static/js/2.171ecf6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f22819b13b7cddb4881",
+    "url": "/geolocation_wether/static/js/main.fb9f76d3.chunk.js"
+  },
+  {
+    "revision": "c955a99458e65f65cedf",
+    "url": "/geolocation_wether/static/js/runtime-main.f5031eed.js"
+  }
+]);
